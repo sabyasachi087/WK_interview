788e07ee41d0decfbb478fa87eabfdba15a6ddae
Sabyasachi Roy Choudhury <schoudhury@ptc.com>
2019-06-01 11:06:48 +0530

    Initial Commit

---
+++ src/main/java/com/wolterskluwer/bca/BasicCollectionsAndAopApplication.java
@@ -0,0 +1,13 @@
+package com.wolterskluwer.bca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCollectionsAndAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCollectionsAndAopApplication.class, args);
+	}
+
+}
